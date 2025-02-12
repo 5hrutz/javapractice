0ae3499e126bc025757680ed43288df15bd9778c
Shruti Raj <shrutiraj7594@gmail.com>
2025-02-12 21:41:08 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,23 @@
+public class Casting {
+    public static void main(String[] args) {
+                int myvalue = 10;
+                long mylong = myvalue;
+                System.out.println(myvalue);
+
+                double mydouble = 1234.5678;
+                float myfloat = 12345.6789F;
+                System.out.println(mydouble);
+
+
+                long mydigit = 12345678999L;
+                int myint = 12345;
+                System.out.println(mydigit);
+
+               
+        
+    }
+}
+        
+    
+    
+
