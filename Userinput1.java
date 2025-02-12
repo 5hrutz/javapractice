02cd63be64d5f21abb1f8f72a20a2a1bdc51f5a0
Shruti Raj <shrutiraj7594@gmail.com>
2025-02-12 22:56:43 +0530

    Add files via upload

---
+++ Userinput1.java
@@ -0,0 +1,13 @@
+import java . util. Scanner;
+public class Userinput1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String Stringfirstname = "Shruti";  
+        String Stringlastname = "Raj";
+        System.out.println(Stringfirstname + Stringlastname);
+        
+    }
+
+
+    
+}
---
+++ anu.java
@@ -0,0 +1,25 @@
+public class anu {
+    public static void main(String[] args) {
+        int variable = 10;
+         int array [] = new int[10];
+
+         array[0] = 12;
+         array[1] = 13;
+         array[2] = 14;
+         array[3] = 15;
+         array[4] = 16;
+         array[5] = 17;
+         array[6] = 18;
+         array[7] = 19;
+         array[8] = 20;
+         array[9] = 2;
+
+
+    System.out.println("This is a variable " + variable); 
+    System.out.println("This is an array " + array[0]);
+    System.out.println("This is an array " + array[1]); 
+    
+
+    }
+    
+}
